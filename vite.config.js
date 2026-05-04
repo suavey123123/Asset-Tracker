b8d8636af52760d22103764062b160eed22c5da0
SuaveyCorp <seehimrun@gmail.com>
2026-05-04 16:18:07 -0700

    hotfix

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    rollupOptions: {
+      output: {
+        // your existing rollup options here (if any)
+      }
+    }
+  },
+
+  // Add this:
+  esbuild: {
+    // This helps avoid eval in some cases
+    legalComments: 'none',
+  }
 })
