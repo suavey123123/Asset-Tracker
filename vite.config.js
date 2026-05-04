06196a8a1564ff3e59f4029385351d0a687b0a82
unknown <seehimrun@gmail.com>
2026-05-04 14:50:08 -0700

    employee blackscreenv7

--- vite.config.js
+++ vite.config.js
@@ -4,3 +4,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        // ...
+      }
+    }
+  },
+  // Add this:
+  esbuild: {
+    // This helps avoid eval in some cases
+    legalComments: 'none',
+  }
+})
